e0ae4acd434c2772913ce1cd07aad62b2e844ef3
BIEMAX <tecnico.beilke@gmail.com>
2022-04-13 17:14:50 -0300

    Created dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  applicationName: 'Financial Manager'
+};
+
+export const userInterface = {
+  controlColors: "primary", //Can be: primary (purple), accent (pink) or warn (orange)
+};
