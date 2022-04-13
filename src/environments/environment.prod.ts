52b8ac574b1aeb5c19e4b3ee9c4d80edae663b2a
BIEMAX <tecnico.beilke@gmail.com>
2022-04-12 22:01:58 -0300

    Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+
+export const userInterface = {
+  controlColors: "primary", //Can be: primary (purple), accent (pink) or warn (orange)
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: false
 };
+
+export const userInterface = {
+  controlColors: "primary", //Can be: primary (purple), accent (pink) or warn (orange)
+};
