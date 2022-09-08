49f9cc9516cc100af1d4aea6b755e74ae9f4072f
BIEMAX <tecnico.beilke@gmail.com>
2022-09-08 20:33:12 -0300

    Added custom dialog component

--- src/app/views/generic/dialog/custom-dialog.component.ts
+++ src/app/views/generic/dialog/custom-dialog.component.ts
@@ -1,17 +1,26 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { DialogFields } from 'src/app/models/dialog-fields.model';
 
 @Component({
   selector: 'custom-dialog.component',
   templateUrl: './custom-dialog.component.html',
   styleUrls: ['./custom-dialog.component.css']
 })
-export class CustomDialogComponent implements OnInit {
+export class CustomDialogComponent {
 
   constructor(public _dialogRef: MatDialogRef<CustomDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public _data: Object) { }
+    @Inject(MAT_DIALOG_DATA) public data: DialogFields[]) { }
 
-  ngOnInit (): void {
+  value: any;
+
+  afterOpened (): void { }
+
+  onNoClick (): void {
+    this._dialogRef.close();
   }
 
+  matDialogClose () {
+    console.log("dioneiiii");
+  }
 }
