d03917330abe96524e43acb612b28b0d60c3e036
Dionei Beilke dos Santos <tecnico.beilke@gmail.com>
2022-09-13 16:48:20 -0300

    Changed mandatory fields and added new util

--- src/app/models/financial.model.ts
+++ src/app/models/financial.model.ts
@@ -1,23 +1,38 @@
 import { EmptyFieldException } from '../exceptions/empty.field.exception'
 
-export class Financial {
+export class FinancialModel {
   id: String;
   user: String;
-  description: String;
+  name: String;
   dueDate: Date;
+  description: String;
   value: Number;
+  quantityAmount: Number;
+  tags: Array<String>;
 
-  constructor(Id: String, User: String, Description: String, DueDate: Date, Value: Number) {
+  constructor(
+    Id: String,
+    User: String,
+    Name: String,
+    DueDate: Date,
+    Description: String,
+    Value: Number,
+    QuantityAmount: Number,
+    Tags: Array<String>
+  ) {
     if (typeof (Id) == "undefined" || !Id) throw new EmptyFieldException(Id);
-    else if (typeof (Description) == "undefined" || !Description) throw new EmptyFieldException(Description);
+    else if (typeof (Name) == "undefined" || !Name) throw new EmptyFieldException(Name);
     else if (typeof (DueDate) == "undefined" || !DueDate) throw new EmptyFieldException(DueDate);
     else if (typeof (Value) == "undefined" || !Value) throw new EmptyFieldException(Value);
     else {
       this.id = Id;
       this.user = User;
-      this.description = Description;
+      this.name = Name;
       this.dueDate = DueDate;
+      this.description = Description;
       this.value = Value;
+      this.quantityAmount = QuantityAmount || 1;
+      this.tags = Tags;
     }
   }
 }
