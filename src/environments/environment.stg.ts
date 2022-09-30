007b61916eba6e9fbd3afc663450dc42bb17cf91
Dionei Beilke dos Santos <tecnico.beilke@gmail.com>
2022-09-30 12:07:12 -0300

    Fixed env for staging with secret

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,1 +1,12 @@
-${{ ANGULAR_STAGING_ENVIRONMENT_DEFINITIONS }}
+export const environment = {
+  production: false,
+  applicationName: 'Financial Manager',
+  apiUrl: '${{ secrets.URL_API_STAGING_ENVIRONMENT }}',
+  apiVersion: 'v1',
+  apiSecret: '',
+  logInfo: true,
+};
+
+export const ui = {
+  color: "primary", //Can be: primary (purple), accent (pink) or warn (orange)
+};
