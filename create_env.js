2377b2bb65f7cbe87eb425d3ed642b5f50ff58f8
Dionei Beilke dos Santos <tecnico.beilke@gmail.com>
2022-10-01 11:35:13 -0300

    Implemented Create secrets for different environments in angular (stage & production) #18

---
+++ create_env.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+const dir = "src/environments";
+
+const defaultFile = `${dir}/environment.ts`;
+const stgFile = "environment.stg.ts"
+const prdFile = "environment.prod.ts"
+
+const content = `${process.env.ENVIRONMENT_CONTENT}`;
+const env = `${process.env.ENVIRONMENT}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.log("1. src doesn't exist, creating now", process.cwd());
+
+    //Create src
+    fs.mkdir(dir, { recursive: true }, (err) => {
+      if (err) throw err;
+    })
+  }
+  //Now, create file
+  try {
+    console.log('2. creating default file');
+    fs.writeFileSync(defaultFile, content);
+    console.log('3. Default file created');
+
+    console.log('4. Env: ', env);
+    if (env === 'stg') fs.writeFileSync(`${dir}/${stgFile}`, content);
+    else fs.writeFileSync(`${dir}/${prdFile}`, content);
+
+    console.log('5. Files created successfully');
+
+    if (fs.existsSync(defaultFile)) {
+      console.log('6. File is created', path.resolve(defaultFile));
+      const str = fs.readFileSync(defaultFile).toString();
+      console.log('7. File content: ', str);
+    }
+
+  }
+  catch (err) {
+    console.log('8. error when creating file: ', err);
+    process.exit(1);
+  }
+});
