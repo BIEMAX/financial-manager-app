edcab1a0ac361354fa1b38fd8c4351a4b2ce9ad3
BIEMAX <tecnico.beilke@gmail.com>
2022-10-25 12:07:22 -0300

    Added missing files

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  applicationName: 'Financial Manager',
+  apiUrl: 'http://localhost:8080',
+  apiVersion: 'v1',
+  apiSecret: 'YOUR_SECRET_HERE',
+  apiClientId: 'YOUR_CLIENT_ID_HERE',
+  logInfo: true,
+};
+
+export const ui = {
+  color: "primary", //Can be: primary (purple), accent (pink) or warn (orange)
+};
