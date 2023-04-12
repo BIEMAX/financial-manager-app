fba74e0b4e9d0575f1ca7dfe7ee7e27732617872
BIEMAX <tecnico.beilke@gmail.com>
2023-04-11 22:54:02 -0300

    Create a new template in alpilean

--- src/app/views/presell/fran/alpilean/alpilean.component.ts
+++ src/app/views/presell/fran/alpilean/alpilean.component.ts
@@ -11,6 +11,8 @@ export class AlpileanComponent implements OnInit {
 
   public uiColor = ui.color;
   public productName: string = "Alpilean";
+  public showLastChanceToBuy: boolean = true;
+  public offerNumber: number = 40;
   private affiliateLink: string = 'https://7d2fdbt4-39r7ke2mcr8kwvybp.hop.clickbank.net/?tid=CB032023';
 
   constructor(private titleService: Title) { }
